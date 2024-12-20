a70875ec353e7d1ab327cd375b71bfe72152f53b
WizzyCodes <wisdomiberi832@gmail.com>
2024-12-20 04:55:55 +0100

    pulled

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+// import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-[#F2F4F7]">
+      <div className="grid w-[70%] h-[75%] grid-cols-2 bg-red-500">
+        <div className="w-[50]"></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
